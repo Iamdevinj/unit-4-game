3eaef4cb8053025db158f2d6bbbd414936658764
Devin Jenkins <dvnjenks@gmail.com>
2019-05-18 22:15:08 -0700

    finished, doesnt work

---
+++ assets/javascript/game.js
@@ -0,0 +1,40 @@
+//random numnber should be generated everytime a new game starts
+//when faces are clicked total score will go up
+//every face should have a value attache between the number 1-12
+
+
+//document ready
+$(document).ready(function() {
+
+
+//at random math counter
+let counter = Math.floor(Math.random() * 101 + 19);
+let randNum1 = Math.floor(Math.random() * 11 + 1);
+let randNum2 = Math.floor(Math.random() * 11 + 1);
+let randNum3 = Math.floor(Math.random() * 11 + 1);
+let randNum4 = Math.floor(Math.random() * 11 + 1);
+
+}
+
+let total = 0;
+let wins = 0;
+let losses = 0;
+
+
+//click functions for pictures 
+$("#arya").on("click",function() {
+
+    
+})
+
+$("#jon").on("click", function () {
+    
+})
+
+$("#dany").on("click", function () {
+    
+})
+
+$("tyrion").on("click", function () {
+    
+})
